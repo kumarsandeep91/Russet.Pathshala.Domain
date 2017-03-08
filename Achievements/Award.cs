ad329d768e3f3ae2fd4d3c6fdeaf585e7a67ad65
russetechsol <admin@russet.in>
2017-03-08 15:02:59 +0530

    Achievements added..

---
+++ Achievements/Award.cs
@@ -0,0 +1,24 @@
+﻿using PathShala.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathShala.Domain.Achievements
+{
+    public class Award : DomainObject
+    {
+        ///<summary>
+        ///Gets or sets a value indicating whether this instance is valid.
+        /// </summary>
+        /// <value><c>true</c> if this instance is valid; otherwise, <c>false</c>.</value>
+        public override bool IsValid
+        {
+            get
+            {
+                return Validate<Award>();
+            }
+        }
+    }
+}
